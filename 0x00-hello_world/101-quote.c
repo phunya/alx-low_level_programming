ac32bbf3e546e3c0238efa255233b02f077210a7
Neo Morakile <morakileneo@gmail.com>
2022-04-10 20:47:11 +0000

    Unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - entry point
+ *
+ * return: Always o (Success)
+ */
+int main(void)
+{
+	white(1,"and that piece of art is useful - Dora Korpar, 2015-10-19", 59);
+	return (1);
+}
