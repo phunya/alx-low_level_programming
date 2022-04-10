bcebbf275f65fb62f99dd8ae6a9cd3455682d252
Neo Morakile <morakileneo@gmail.com>
2022-04-10 13:17:03 +0000

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: printing thefuntion
- *     */
+ *main - Entry point
+ *
+ *Return: printing thefuntion
+ *  
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
