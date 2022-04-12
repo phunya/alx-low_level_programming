2e4a1b049364016319ea089e466cbfa437fd72ed
Neo Bethuel Morakile <morakileneo@gmail.com>
2022-04-12 08:45:27 +0200

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,11 +13,13 @@ for (d = 0; d < 90; d++)		{
 for ()
 {
 putchar((d / 10) + '0');
-putchar((d % 10) + '0');												}
+putchar((d % 10) + '0');				
+}
 if (d != 89)
 {
 putchar(',');
-putchar(' ');														}
+putchar(' ');														
+}
 }
 putchar('\n');
 return (0);
