ec6f0ac60f08d3aee731b77adbed4520401ad1c7
Neo Morakile <morakileneo@gmail.com>
2022-04-12 06:53:19 +0000

    100 AD Task

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,18 +10,18 @@ int main(void)
 {
 int d;
 for (d = 0; d < 90; d++)
-	{
+{
 for ()
-		{
+{
 putchar((d / 10) + '0');
 putchar((d % 10) + '0');
-		}
+}
 if (d != 89)
-		{
+{
 putchar(',');
 putchar(' ');
-		}
-	}
+}
+}
 putchar('\n');
 return (0);
 }
