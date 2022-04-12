a1218b1ce3e8d04fef4855535eda2ae50eca0d0b
Neo Morakile <morakileneo@gmail.com>
2022-04-12 06:06:03 +0000

    tast 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,9 +8,9 @@
  */
 int main(void)
 {
-int i;
-for (i = 0; i < 10; i++)
-printf("%d", i);
-printf("\n");
-return (0);
+	int i;
+	for (i = 0; i < 10; i++)
+	printf("%d", i);
+	printf("\n");
+	return (0);
 } 
