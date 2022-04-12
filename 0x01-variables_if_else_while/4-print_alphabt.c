ce0afc60487275759bc76a5f9d5dbed4382dde0b
Neo Morakile <morakileneo@gmail.com>
2022-04-12 05:54:27 +0000

    tast 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - enrty point
  *
  * Return: always 0 (Success)
  */
-
 int main(void)
 {
 char low, e, q;
 
-e = 'e';
-q = 'q';
+	e = 'e';
+	q = 'q';
 
-for (low = 'a'; low <= 'z'; low++)
+	for (low = 'a'; low <= 'z'; low++)
 	{
 		if (low != e && low != q)
 		putchar(low);
