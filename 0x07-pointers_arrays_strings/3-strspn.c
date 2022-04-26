f21f81bab1e09dd5bb8ad4f20bfc17a72a157382
Neo Bethuel Morakile <morakileneo@gmail.com>
2022-04-26 03:17:48 +0200

    3-strspn.c task

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,9 +1,7 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _strspn - Entry point
- * @s: input
- * @accept: input
  * Return: Always 0 (Success)
  */
 unsigned int _strspn(char *s, char *accept)
