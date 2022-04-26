e32f2b7236b36163639c263b75f5e6b7f8d4142b
Neo Bethuel Morakile <morakileneo@gmail.com>
2022-04-26 03:16:36 +0200

    2-strchr.c task

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _strchr - Entry point
- * @s: input
- * @c: input
  * Return: Always 0 (Success)
  */
 char *_strchr(char *s, char c)
