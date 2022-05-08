a98dd71f3e75edba3daa184e78b2ebe7cd6c68a0
Neo Bethuel Morakile <morakileneo@gmail.com>
2022-05-08 13:49:56 +0200

    3-factorial.c Task

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * Youtube
- * @n: input
- * Return: Always 0 (Success)
+ * factorial - gets factorial of n
+ * @n: integer
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
-	else if (n == 0)
+	if (n == 0)
 		return (1);
-	else
-		return (n * factorial(n - 1));
+	return (n * factorial(n - 1));
 }
