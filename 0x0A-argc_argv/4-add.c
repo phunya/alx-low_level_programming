6205d591ec39f0e7871628198b1a30f199f6ff7f
Neo Bethuel Morakile <morakileneo@gmail.com>
2022-05-08 22:45:37 +0200

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,20 +9,21 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, j, add = 0;
+	int i, sum = 0;
+
+	if (argc < 1)
+		return (0);
 
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		if (!atoi(argv[i]))
 		{
-			if (!isdigit(argv[i][j]))
-			{
-				printf("Error\n");
-				return (1);
-			}
+			printf("%s\n", "Error");
+			return (1);
 		}
-		add += atoi(argv[i]);
+		sum += atoi(argv[i]);
 	}
-	printf("%d\n", add);
+	printf("%d\n", sum);
+
 	return (0);
 }
