c1b18d275492d4def4f31fdbd03b8d61244b220d
Neo Bethuel Morakile <morakileneo@gmail.com>
2022-05-27 03:05:56 +0200

    100-get_endianness.c Task

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -5,11 +5,8 @@
  */
 int get_endianness(void)
 {
-	unsigned int i;
+	unsigned int i = 1;
+	char *c = (char *) &i;
 
-	i = 1;
-	if (*(char *)&i == 1)
-		return (1);
-	else
-		return (0);
+	return (*c);
 }
