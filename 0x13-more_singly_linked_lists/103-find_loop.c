a36ef000659a043a2b1971b85abf0e2dbf077d7e
Neo Bethuel Morakile <morakileneo@gmail.com>
2022-05-31 04:39:46 +0200

    103-find_loop.c update Task

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,32 +1,34 @@
 #include "lists.h"
 
 /**
- * find_listint_loop - finds the loop in a linked list.
- * @head: pointer to the beginning of the list
+ * find_listint_loop - finds the loop in a linked list
+ * @head: linked list to search for
  *
- * Return: address of the node where the loop starts or NULL if there's no loop
+ * Return: address of the node where the loop starts, or NULL
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *current, *next_node;
+	listint_t *slow = head;
+	listint_t *fast = head;
 
-	current = next_node = head;
-	while (current && next_node && next_node->next)
+	if (!head)
+		return (NULL);
+
+	while (slow && fast && fast->next)
 	{
-		current = current->next;
-		next_node = next_node->next->next;
-		if (current == next_node)
+		fast = fast->next->next;
+		slow = slow->next;
+		if (fast == slow)
 		{
-			current = head;
-			break;
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (fast);
 		}
 	}
-	if (!current || !next_node || !next_node->next)
-		return (NULL);
-	while (current != next_node)
-	{
-		current = current->next;
-		next_node = next_node->next;
-	}
-	return (next_node);
+
+	return (NULL);
 }
