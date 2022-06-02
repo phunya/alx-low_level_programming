aed2d9c0a4117985a12190f667e084478457da8f
Neo Bethuel Morakile <morakileneo@gmail.com>
2022-06-02 23:08:39 +0200

    103-infinite_add.c Task Up

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,48 +1,45 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
- * print_buffer - prints buffer of a char string
- * @b: buffer to print
- * @size: size of buffer
- * Return: nothing
+ * infinite_add - adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer for result
+ * @size_r: buffer size
+ *
+ * Return: address of r or 0
  */
-
-void print_buffer(char *b, int size)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j;
-
-	if (size <= 0)
-		printf("\n");
-	else
-	{
-		for (i = 0; i < size; i += 10)
-		{
-			printf("%08x: ", i);
-			j = 0;
-			while (j < 10)
-			{
-				if (j % 2 == 0 && j != 0)
-					printf(" ");
-				if ((j + i) > size - 1)
-				{
-					printf("  ");
-				}
-				else
-					printf("%.2x", b[i + j]);
-				j++;
-			}
-			printf(" ");
-			j = 0;
-			while (j < 10)
-			{
-				if ((j + i) > size - 1)
-					break;
-				if (b[j + i] <= 31 || b[j + 1] >= '~')
-					b[j + i] = '.';
-				putchar(b[j + i]);
-				j++;
-			}
-			printf("\n");
-		}
-	}
+int i, j, k, l, m, n;
+for (i = 0; n1[i]; i++)
+;
+for (j = 0; n2[j]; j++)
+;
+if (i > size_r || j > size_r)
+return (0);
+m = 0;
+for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
+{
+n = m;
+if (i >= 0)
+n += n1[i] - '0';
+if (j >= 0)
+n += n2[j] - '0';
+if (i < 0 && j < 0 && n == 0)
+{
+break;
+}
+m = n / 10;
+r[k] = n % 10 + '0';
+}
+r[k] = '\0';
+if (i >= 0 || j >= 0 || m)
+return (0);
+for (k -= 1, l = 0; l < k; k--, l++)
+{
+m = r[k];
+r[k] = r[l];
+r[l] = m;
+}
+return (r);
 }
