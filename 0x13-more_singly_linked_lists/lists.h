6eb775d4cfa7735d05fd2ee7b6611a1abf8ee3de
Neo Bethuel Morakile <morakileneo@gmail.com>
2022-06-02 23:37:38 +0200

    lists.h updated

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -3,8 +3,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
+#define SUCCESS 1
+#define FAILURE -1
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -32,5 +33,8 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
+size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
 
-#endif
+#endif /* LISTS_H */
