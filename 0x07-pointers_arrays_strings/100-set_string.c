dc9531eefdea509178c2227fddbfc3280bccfe19
Neo Bethuel Morakile <morakileneo@gmail.com>
2022-06-02 23:16:25 +0200

    100-set_string.c Task

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,13 +1,11 @@
 #include "main.h"
-
 /**
- * set_string - prints buffer in hexa
- * @s: cheese
- * @to: buffer2
- *
- * Return: Nothing.
- */
+*   set_string - set value of pointer to a char
+*    @s: variable of type pointer
+*     @to: char
+*/
+
 void set_string(char **s, char *to)
 {
-  *s = to;
+*s = to;
 }
