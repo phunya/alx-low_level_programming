8e77dece3910621b60b0fc167080a4c292c09967
Neo Bethuel Morakile <morakileneo@gmail.com>
2022-06-09 17:35:40 +0200

    main.h task

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,19 +1,19 @@
-#ifndef MAIN
-#define MAIN
+#ifndef MAIN_H
+#define MAIN_H
 
-/*
- * Desc: Header file containing prototypes for all functions
- *       written in the 0x14-file_io directory.
- */
+/* library files*/
 
+#include <stdlib.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <unistd.h>
+#include <stdio.h>
 
-int _putchar(char c);
+/*function prototypes*/
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif /* MAIN_H */
+#endif
