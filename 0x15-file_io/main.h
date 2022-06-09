9310fe52c5ddedb58b37aec0f6a0f7f1bd6de9df
Neo Bethuel Morakile <morakileneo@gmail.com>
2022-06-09 17:32:57 +0200

    main.h test

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,10 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAIN
+#define MAIN
+
+/*
+ * Desc: Header file containing prototypes for all functions
+ *       written in the 0x14-file_io directory.
+ */
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -11,4 +16,4 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif
+#endif /* MAIN_H */
